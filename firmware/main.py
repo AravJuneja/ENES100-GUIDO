3e38240218f1b5275d017b2493f3d0d4cafcbd83
ztariq12-create <ztariq12@terpmail.umd.edu>
2025-11-24 15:24:57 -0500

    Update main.py
    Added in some comments for the pin configs of the ultrasonic sensor

--- firmware/main.py
+++ firmware/main.py
@@ -5,7 +5,6 @@ main.py acts as the mission controller.
 from motor_control import MotorControl
 
 
-
 # Pin Configs for Motors 
 PIN_IN1 = 19
 PIN_IN2 = 18
@@ -14,12 +13,19 @@ PIN_IN3 = 5
 PIN_IN4 = 17
 PIN_ENB = 16
 
+#Pin Configs for Scooper Ultrasonic Sensor
+#Trig_One = 12
+#Echo_One = 13
 
+#Pin Configs for Back Ultrasonic Sensor 
+#Trig_Two = 2
+#Echo_Two = 4
 
 motors = MotorControl(PIN_IN1, PIN_IN2, PIN_ENA, PIN_IN3, PIN_IN4, PIN_ENB)
 
 motors.forward(100)
 sleep(5)
 motors.stop()
+
 
 
